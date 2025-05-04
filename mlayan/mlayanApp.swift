e63704b81b892e956997ced4475ced56f017fab0
shaden <shaden.wasaidi@gmail.com>
2025-05-04 21:04:08 +0300

    Initial Commit

---
+++ mlayan/mlayanApp.swift
@@ -0,0 +1,17 @@
+//
+//  mlayanApp.swift
+//  mlayan
+//
+//  Created by shaden  on 06/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct mlayanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mlayanTests/mlayanTests.swift
@@ -0,0 +1,17 @@
+//
+//  mlayanTests.swift
+//  mlayanTests
+//
+//  Created by shaden  on 06/11/1446 AH.
+//
+
+import Testing
+@testable import mlayan
+
+struct mlayanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
